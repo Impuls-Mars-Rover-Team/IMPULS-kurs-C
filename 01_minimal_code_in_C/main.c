d7e7a66bce4b7ff4ad152eb186877073d6f39c3d
Grzegorz Witkowski <gwitkowski2000@gmail.com>
2023-03-18 20:07:32 +0100

    dodane komentarze

--- 01_minimal_code_in_C/main.c
+++ 01_minimal_code_in_C/main.c
@@ -1,15 +1,14 @@
+/* Biblioteka standardowych wejść / wyjść - zawiera funkcję printf*/
 #include <stdio.h>
 
-int variableName = 9;
-float floatVariable = 3.14;
-double doubleVariable = 3.98;
-char charVariable = 'a';
-
-
+/* Funkcja main - tu dzieje się większość operacji 
+ * int - typ zmiennej zwracanej przez funkcję [Na razie to nas nie obchodzi]
+ * void - typ (pusty) argumentu przyjmowanego przez funkcję [Na razie to nas nie obchodzi]*/
 int main(void){
 
-    printf("%c", charVariable);
+    /* funkcja wyświetlająca w konsoli łańcuch znaków */
+    printf("Hello IMPULS candidate\r\n");
 
-
+    /* wartość zwracana przez funkcję main [na razie to nas nie obchodzi]*/
     return 0;   
 }
