9aeb2f3c8c22fa12422072ed58fc8684f31a4f31
Grzegorz Witkowski <gwitkowski2000@gmail.com>
2023-03-18 13:43:50 +0100

    timer using Windows.h and stdlib.h

--- 00_test/main.c
+++ 00_test/main.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
+#include <Windows.h>
+#include <stdlib.h>
+
+int seconds = 0;
+int minutes = 0;
+int hours = 0;
+int blinkPeriod = 250;
 
 int main(void){
 
-    printf("\r\nIMPULS sekcja elektroniki - Kurs programowania w jezyku C\r\n\n");
+    for(int i = 0; i < 5; i++){
+        printf("IMPULS sekcja elektroniki - Kurs programowania w jezyku C\r\n\n");
+        Sleep(blinkPeriod);
+        system("cls");
+        Sleep(blinkPeriod);
+    }
+    
+    while(1){
+        system("cls");
+        
+        if(seconds < 59){
+            seconds++;
+        }else{
+            minutes++;
+            seconds = 0;
+        }
+
+        if((minutes < 59) && (seconds > 59)){
+            minutes++;
+        }else if(minutes > 59){
+            hours++;
+            minutes = 0;
+        }
+
+        printf("Time running: \t%dh\t%dmin\t%ds\r\n", hours, minutes, seconds);
+        Sleep(1000);
+        
+    }
 
     return 0;
 }
