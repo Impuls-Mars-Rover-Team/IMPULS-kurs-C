232dd3a0ac3b8368a29bd2acb408c249c2f0c83d
Grzegorz Witkowski <gwitkowski2000@gmail.com>
2023-03-26 14:11:33 +0200

    teoria

---
+++ 05_functions/main.c
@@ -0,0 +1,36 @@
+/*
+ * IMPULS kurs C - pętle
+ * Funkcje
+ * @gwitkowski2000 2023
+ */
+
+/*
+ * Funkcja to wydzielona część programu, która przetwarza argumenty (dane wejściowe),
+ * wykonuje jakieś operacje (najczęściej na argumentach albo zależne od argumentów)
+ * i ewentualnie zwraca wartość (najczęściej będącą wynikiem tych operacji).
+ * 
+ * Składnia:
+ * 
+ * deklaracja funkcji: **arg - argument
+ * typZwracany nazwaFunkcji(typArg1 nazwaArg1, typArgg2, nazwaArgg2, ..., typArgN, nazwaArgN);
+ * 
+ * definicja funkcji:
+ * typZwracany nazwaFunkcji(typArg1 nazwaArg1, typArg2, nazwaArg2, ..., typArgN, nazwaArgN){
+ *   //operacje na argumentach - dodawanie, mnożenie, całkowanie, porównywanie itp, np:
+ *   int zmiennaPomocnicza = nazwaArg1 + nazwaArg2;
+ *   int wynik = zmiennaPomocnicza * nazwaArgN;
+ *   
+ *   return wynik;   
+ * 
+ * }
+ * 
+ * słowo kluczowe return powoduje zwrócenie przez funkcję zmiennej wynik.
+ * Zmienna zwracana musi być tego samego typu co typ funkcji
+ * 
+ * Wywołanie funkcji:
+ * typZmiennej nazwaZmiennej = nazwaFunkcji(Argument1, Argument2, ..., ArgumentN);
+ * 
+ * innaFunkcja(nazwaFunkcj(Argument1, Argument2, ..., ArgumentN));
+ * 
+ * */
+
