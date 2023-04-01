32f38bb7f624f7d01b2abca6eb3ffc9da83cce2d
Grzegorz Witkowski <gwitkowski2000@gmail.com>
2023-04-01 16:56:36 +0200

    teoria

---
+++ 10_customVariableTypes/main.c
@@ -0,0 +1,14 @@
+/*
+ * IMPULS kurs C
+ * Wlasne typy zmiennych
+ * @gwitkowski2000 2023
+ */
+
+/* Jesli mamy taką potrzebę, nie podoba nam się pisanie struct nazwaStruktury Struktura przy tworzeniu struktur albo 
+ * chcemy ztworzyć własny typ zwracający np, kod błędu, możemy stworzyć własne typy, używając instrukcji typedef 
+ *
+ * Składnia:
+   
+typedef oryginalnaNazwaTypu nowaNazwaTypu;
+ 
+ */
