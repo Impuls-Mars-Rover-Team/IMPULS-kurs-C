feb40218a35e4919fda8fce471fb96489b8eb577
Grzegorz Witkowski <gwitkowski2000@gmail.com>
2023-04-01 16:48:35 +0200

    teoria

---
+++ 09_enumerations/main.c
@@ -0,0 +1,31 @@
+/*
+ * IMPULS kurs C
+ * Wyliczenia
+ * @gwitkowski2000 2023
+ */
+
+
+/* Wyliczenia służą do przypisywania wartości liczbowej wyrażeniom. 
+ * Mogą służyć do definiowania stałych liczbowych i do tworzenia własnych typów
+ *
+ * Składnia:
+ * 
+enum nazwaWyliczenia{
+    pierwszeWyrazenie = 1,
+    drugieWyrazenie,
+    trzecieWyrazenie,
+};
+
+ * Pierwsze wyrazenie nie musi miec wartosci 1 - przykładowo jesli będzie równe 3,
+   wtedy 3 wyrazenie będzie równe 5
+
+ * Można przypisać każdemu wyrażeniu wartość "na sztywno"
+
+ enum nazwaWyliczenia{
+    pierwszeWyrazenie = 11,
+    drugieWyrazenie = 22,
+    trzecieWyrazenie = 33,
+};
+
+ */
+
